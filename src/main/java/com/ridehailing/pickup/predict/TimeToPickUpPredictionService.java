e5d88780af903d9525f6615494a127b293c4355a
amithn <amith.nmbr@gmail.com>
2019-03-26 11:17:08 +1100

    Adjusting delay

--- src/main/java/com/ridehailing/pickup/predict/TimeToPickUpPredictionService.java
+++ src/main/java/com/ridehailing/pickup/predict/TimeToPickUpPredictionService.java
@@ -10,7 +10,7 @@ import java.util.Random;
 @Timed
 public class TimeToPickUpPredictionService {
     private Random randomGen = new Random();
-    private int FIXED_DELAY_MS = 250;
+    private int FIXED_DELAY_MS = 200;
 
     @Timed("prediction.time")
     public int predict() {
