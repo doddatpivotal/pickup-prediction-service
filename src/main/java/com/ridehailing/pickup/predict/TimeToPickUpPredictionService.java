bc91bb9e9e1a79a1559d49a992f38257e62ccbb7
amithn <amith.nmbr@gmail.com>
2019-04-02 12:27:58 -0400

    Adding new ML algo for predicting pickup time.

--- src/main/java/com/ridehailing/pickup/predict/TimeToPickUpPredictionService.java
+++ src/main/java/com/ridehailing/pickup/predict/TimeToPickUpPredictionService.java
@@ -10,7 +10,7 @@ import java.util.Random;
 @Timed
 public class TimeToPickUpPredictionService {
     private Random randomGen = new Random();
-    private int PREDICTION_TIME_MS = 250;
+    private int PREDICTION_TIME_MS = 210;
 
     @Timed("prediction.time")
     public int predict() {
