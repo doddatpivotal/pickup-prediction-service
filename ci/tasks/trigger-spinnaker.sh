d5605cd2730aef8e44be70b2d54a9fa87dc48454
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-08-23 23:22:43 -0400

    YOLO

---
+++ ci/tasks/trigger-spinnaker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export version=`cat version/version`
+echo "Build version: ${version}"
+
+curl -X POST \
+  ${SPINNKER_API}/webhooks/webhook/${WEBHOOK_SOURCE} \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  -d '{
+	"artifacts": [
+		{
+        "reference": "${GROUP_ID}:${ARTIFACT_ID}:${version}",
+        "type": "maven/file"
+        }
+	]
+}
+' -k
+
+# Create github tag file to be used in later put step
+echo "${version}-spinnaker-trigger-$(date +%Y%m%d_%H%M%S)" > tag.txt
