2064e4c8ea62c7c3cf90d3b5062e1ce083f1c464
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-08-24 11:21:13 -0400

    YOLO

--- ci/tasks/build-and-publish.sh
+++ ci/tasks/build-and-publish.sh
@@ -5,25 +5,7 @@ set -ex
 export version=`cat version/version`
 echo "Build version: ${version}"
 
-M2_HOME=${HOME}/.m2
-mkdir -p ${M2_HOME}
-
-export ROOT_FOLDER=$( pwd )
-M2_LOCAL_REPO="${ROOT_FOLDER}/.m2"
-
-mkdir -p "${M2_LOCAL_REPO}/repository"
-
-cat > ${M2_HOME}/settings.xml <<EOF
-
-<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
-      <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
-</settings>
-
-EOF
-echo "Settings xml written"
+./ci-scripts/ci/tasks/create-maven-settings-xml.sh
 
 cd code-repo
 
@@ -39,10 +21,10 @@ export BUILD_URI=${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_
 echo "Running mvn deploy command"
 ./mvnw versions:set \
     -DnewVersion=${version} \
-    -s ${M2_HOME}/settings.xml
+    -s ${HOME}/.m2/settings.xml
 ./mvnw deploy \
     -DskipTests \
-    -s ${M2_HOME}/settings.xml
+    -s ${HOME}/.m2/settings.xml
 
 # Create file with tag name to be used in later put step
 echo "version-${version}-artifactory-deploy-$(date +%Y%m%d_%H%M%S)" > ../results/tag.txt
