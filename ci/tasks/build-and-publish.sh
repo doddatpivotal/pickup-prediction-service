e2a20beeec79dbaf0fc430f8e746ee6776f56a76
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-08-24 11:00:47 -0400

    YOLO

--- ci/tasks/build-and-publish.sh
+++ ci/tasks/build-and-publish.sh
@@ -32,6 +32,7 @@ echo "CODE_CONTEXT_URL: ${REPO_CONTEXT_URL}"
 export BUILD_ID=`cat ${ROOT_FOLDER}/meta/build-id`
 export BUILD_TEAM_NAME=`cat ${ROOT_FOLDER}/meta/build-team-name`
 export BUILD_PIPELINE_NAME=`cat ${ROOT_FOLDER}/meta/build-pipeline-name`
+export ATC_EXTERNAL_URL=`cat ${ROOT_FOLDER}/meta/atc-external-url`
 export BUILD_URI=${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}
 
 echo "BUILD_ID: ${BUILD_ID}"
